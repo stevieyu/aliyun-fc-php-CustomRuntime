c53f54beee6d8c9fa118dcad0e0e1da262b57ddb
Stevie <yzm629@sina.cn>
2020-03-27 15:02:39 +0800

    update

--- .fun/nas/1213e74b022-hag27.cn-hongkong.nas.aliyuncs.com/php-swoole/laravel/routes/web.php
+++ .fun/nas/1213e74b022-hag27.cn-hongkong.nas.aliyuncs.com/php-swoole/laravel/routes/web.php
@@ -13,11 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/2016-08-15/proxy/customruntime/php-swoole', function () {
+//     return view('welcome');
+// });
 
-Route::get('/2016-08-15/proxy/customruntime/php-swoole', function () {
-    return view('welcome');
-});
 
-Route::get('/', function () {
+Route::get('/{any?}', function () {
     return view('welcome');
-});
+})->where(['capture' => '.*']);
+
+Route::get('/p0/{any?}', function () {
+    return view('welcome');
+})->where(['capture' => '.*']);
